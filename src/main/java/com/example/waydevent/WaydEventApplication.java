24de0c0ff68f94cefe65a8a26142b7667be470b6
TSVlad <vladislav.kolmykov@yandex.ru>
2021-10-05 22:31:30 +0300

    Initial commit

---
+++ src/main/java/com/example/waydevent/WaydEventApplication.java
@@ -0,0 +1,13 @@
+package com.example.waydevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaydEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaydEventApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/waydevent/WaydEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waydevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaydEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
